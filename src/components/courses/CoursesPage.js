36d03142022e33eb3d8da251c324da9d8cb3fd3d
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-09-24 22:42:48 +0300

    [task] use arrow functions in CoursePage class component to avoid messing with THIS keyword

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
 
 class CoursesPage extends Component {
+  state = {
+    course: {
+      title: ""
+    }
+  };
+
+  handleChange = ({ target: { value } }) => {
+    const course = { ...this.state.course, title: value };
+    this.setState({ course });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
