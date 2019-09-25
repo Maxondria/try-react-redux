342fd98365b4edd5e6995af66e9a0f738a7b2f50
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-09-25 18:58:47 +0300

    [task] add initial state to reducers

--- src/redux/reducers/authorReducer.js
+++ src/redux/reducers/authorReducer.js
@@ -1,6 +1,7 @@
 import actionTypes from "../actions/actionTypes";
+import initialState from "./initialState";
 
-export default function authorReducer(state = [], action) {
+export default function authorReducer(state = initialState.authors, action) {
   switch (action.type) {
     case actionTypes.LOAD_AUTHORS_SUCCESS:
       return action.authors;
