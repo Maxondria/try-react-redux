50fb86ff2803cdc623ce3065c66cee87e80e9066
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-09-25 23:01:18 +0300

    [task] restructure ManageCourse Component to use hooks - functional

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,30 +1,26 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { loadCourses } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 
-class MangeCoursePage extends Component {
-  componentDidMount() {
-    const { courses, authors, loadCourses, loadAuthors } = this.props;
-
+const MangeCoursePage = ({ courses, authors, loadCourses, loadAuthors }) => {
+  useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch(error => alert("Error Loading Courses: " + error));
     }
 
     if (authors.length === 0) {
       loadAuthors().catch(error => alert("Error Loading Authors: " + error));
-    }
-  }
+    } 
+  }, []);
 
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  );
+};
 
 MangeCoursePage.propTypes = {
   loadCourses: PropTypes.func.isRequired,
@@ -44,4 +40,3 @@ export default connect(
   mapStateToProps,
   MapDispatchToProps
 )(MangeCoursePage);
-
